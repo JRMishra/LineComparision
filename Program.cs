3a6683ab42dcd578e4b53e9989eb74bf78dc78c8
JR Mishra <jrmnitdgp@gmail.com>
2020-09-26 22:02:02 +0530

    [JR Mishra] Add. Line Comparision Computation problem initialized

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace LineComparision
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Line Comparision Computation");
+            Console.WriteLine("===========================================");
+        }
+    }
+}
